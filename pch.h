21d0e55cd20520160c3bedf0bb9efa30682b5a8a
Mrflo67 <47458412+Mrflo67@users.noreply.github.com>
2019-03-01 16:02:34 +0100

    Add files via upload

---
+++ pch.h
@@ -0,0 +1,33 @@
+/**
+*	PCH.H FILE
+*	PRECOMPILE HEADERS TO SPEED UP THE COMPILATION
+*/
+
+#pragma once
+
+/* OpenGL Libraries */
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+/* IMGUI Include */
+#include <IMGUI/imgui.h>
+#include <IMGUI/imgui_impl_glfw_gl3.h>
+
+/* CPP Includes */
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <ctime>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <sys/stat.h>
+#include <vector>
+#include <algorithm>
+
+#include "Header/tinyfiledialogs.h"
